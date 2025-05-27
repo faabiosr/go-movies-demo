4c6c4d3d09ca0d2575c74b2cca3c30c3b4e6d20b
Fabio Ribeiro <faabiosr@gmail.com>
2025-05-27 08:33:49 +0200

    chore(middleware): custom error handling
    A custom Echo HTTPErrorHandler to deal with Public errors and HTTP
    errors.

---
+++ pkg/middleware/error.go
@@ -0,0 +1,73 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/labstack/echo/v4"
+
+	"github.com/faabiosr/go-movies-demo/pkg/errors"
+)
+
+// ErrorHandler handles the default echo error to dispatch in json format.
+func ErrorHandler(err error, ec echo.Context) {
+	if err == nil {
+		return
+	}
+
+	status, msg := inspectErr(err)
+
+	if ec.Response().Committed {
+		return
+	}
+
+	if ec.Request().Method == http.MethodHead {
+		_ = ec.NoContent(status)
+		return
+	}
+
+	_ = ec.JSON(status, echo.Map{"message": msg})
+}
+
+func inspectErr(err error) (int, string) {
+	return status(err), message(err)
+}
+
+func status(err error) int {
+	var he interface {
+		HTTPStatus() int
+	}
+
+	if errors.As(err, &he) {
+		return he.HTTPStatus()
+	}
+
+	var ve validation.Errors
+
+	if errors.As(err, &ve) {
+		return http.StatusBadRequest
+	}
+
+	if ee := new(echo.HTTPError); errors.As(err, &ee) {
+		return ee.Code
+	}
+
+	return http.StatusInternalServerError
+}
+
+func message(err error) string {
+	var pe interface {
+		Public() string
+	}
+
+	if errors.As(err, &pe) {
+		return pe.Public()
+	}
+
+	if ee := new(echo.HTTPError); errors.As(err, &ee) {
+		return fmt.Sprintf("%v", ee.Message)
+	}
+
+	return err.Error()
+}
---
+++ pkg/middleware/error_test.go
@@ -0,0 +1,142 @@
+package middleware
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/labstack/echo/v4"
+
+	"github.com/faabiosr/go-movies-demo/pkg/errors"
+)
+
+func TestErrorHandler(t *testing.T) {
+	tests := []struct {
+		name    string
+		method  string
+		err     error
+		status  int
+		content string
+	}{
+		{
+			name:   "no error",
+			err:    nil,
+			status: http.StatusOK,
+		},
+		{
+			name:    "standard error",
+			err:     errors.New("standard"),
+			status:  http.StatusInternalServerError,
+			content: `{"message":"standard"}`,
+		},
+		{
+			name:   "no content",
+			method: http.MethodHead,
+			err:    errors.New("foo"),
+			status: http.StatusInternalServerError,
+		},
+		{
+			name:    "echo error",
+			err:     &echo.HTTPError{Code: http.StatusBadRequest, Message: "echo error"},
+			status:  http.StatusBadRequest,
+			content: `{"message":"echo error"}`,
+		},
+		{
+			name:    "http error",
+			err:     errors.HTTP(http.StatusNotFound, "event was not found"),
+			status:  http.StatusNotFound,
+			content: `{"message":"event was not found"}`,
+		},
+
+		{
+			name:    "public error",
+			err:     errors.Public(errors.New("failed"), "unable to store"),
+			status:  http.StatusInternalServerError,
+			content: `{"message":"unable to store"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.method == "" {
+				tt.method = http.MethodGet
+			}
+
+			req := httptest.NewRequest(tt.method, "/", nil)
+			rec := httptest.NewRecorder()
+			ec := echo.New().NewContext(req, rec)
+
+			ErrorHandler(tt.err, ec)
+
+			res := rec.Result()
+
+			if code := res.StatusCode; code != tt.status {
+				t.Errorf("expected status code '%d', got %d", tt.status, code)
+			}
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Error(err)
+			}
+
+			content := strings.TrimSpace(string(body))
+
+			if strings.Compare(content, tt.content) != 0 {
+				t.Errorf("expected message '%s', got '%s'", tt.content, content)
+			}
+		})
+	}
+
+	t.Run("committed", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+		ec := echo.New().NewContext(req, rec)
+		ec.Response().WriteHeader(http.StatusBadRequest)
+
+		ErrorHandler(errors.New("failed"), ec)
+
+		res := rec.Result()
+
+		if code := res.StatusCode; code != http.StatusBadRequest {
+			t.Errorf("expected status code '%d', got %d", http.StatusBadRequest, code)
+		}
+
+		if res.ContentLength >= 0 {
+			t.Error("expected empty content")
+		}
+	})
+
+	t.Run("validation error", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+		ec := echo.New().NewContext(req, rec)
+
+		c := struct {
+			Name string
+		}{}
+		ErrorHandler(validation.Errors{
+			"name": validation.Validate(c.Name, validation.Required),
+		}, ec)
+
+		res := rec.Result()
+
+		if code := res.StatusCode; code != http.StatusBadRequest {
+			t.Errorf("expected status code '%d', got %d", http.StatusBadRequest, code)
+		}
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		content := strings.TrimSpace(string(body))
+		expected := `{"message":"name: cannot be blank."}`
+
+		if strings.Compare(content, expected) != 0 {
+			t.Errorf("expected message '%s', got '%s'", expected, content)
+		}
+	})
+}
